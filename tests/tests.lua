39d730d5e5d7599839034a040297247e200506a2
ccgargantua <ccgargantua@proton.me>
2026-01-06 16:08:26 -0500

    Added unit tests and luaunit library

---
+++ tests/tests.lua
@@ -0,0 +1,29 @@
+lu = require('luaunit')
+local beyond = dofile("../1beyond.qplug")
+
+function TestBase64Encoding()
+    local decoded  = "Pi = 3.1415 if you like precision!"
+    local expected = "UGkgPSAzLjE0MTUgaWYgeW91IGxpa2UgcHJlY2lzaW9uIQ=="
+    lu.assertEquals(expected, to_base64(decoded))
+end
+
+function TestUNAPEncode_Basic()
+    local uname = "JohnSmith"
+    local pwd   = "password"
+    local expected = "Sm9oblNtaXRoOnBhc3N3b3Jk"
+    lu.assertEquals(expected, UNAPEncode(uname, pwd))
+end
+
+function TestUNAPEncode_Fail_NoUname()
+    lu.assertError(function()
+        UNAPEncode("", "password")
+    end)
+end
+
+function TestUNAPEncode_Fail_NoPWD()
+    lu.assertError(function()
+        UNAPEncode("JohnSmith", "")
+    end)
+end
+
+os.exit( lu.LuaUnit.run() )
